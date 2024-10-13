81f9f75830f79920ded418553c0e8b68c6e705ad
danielrodrigues <drodrigues@isolutions.com.br>
2024-10-14 00:05:08 +0100

    Spring Security
    - Create the user entity
    - Create the Role entity
    - Implement user and role repositories
    - Create the Token entity and repository
    - Implement the security configuration class
    - Implement the Authentication provider

---
+++ src/main/java/com/drodrigues/book_network/security/JwtFilter.java
@@ -0,0 +1,4 @@
+package com.drodrigues.book_network.security;
+
+public class JwtFilter {
+}
---
+++ src/main/java/com/drodrigues/book_network/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.drodrigues.book_network.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+
+    private final AuthenticationProvider authenticationProvider;
+    private JwtFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(
+                                        "/auth/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/swagger-ui/**",
+                                        "/swagger-ui.html",
+                                        "/v2/api-docs",
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/webjars/**"
+                                ).permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
